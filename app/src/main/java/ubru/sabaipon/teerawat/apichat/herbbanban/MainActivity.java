59480765410e53a3fd8de42dbd4ce22a403e527a
teera <Thira20172017@gmail.com>
2016-03-26 13:33:56 +0700

    ออกเเบบนะ

--- app/src/main/java/ubru/sabaipon/teerawat/apichat/herbbanban/MainActivity.java
+++ app/src/main/java/ubru/sabaipon/teerawat/apichat/herbbanban/MainActivity.java
@@ -1,7 +1,9 @@
 package ubru.sabaipon.teerawat.apichat.herbbanban;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }   // main method
+
+    public void ClickSignInMain(View view) {
+
     }
-}
+
+    public void ClickSignUpMain(View view) {
+        startActivity(new Intent(MainActivity.this, SignUpActivity.class));
+    }
+
+    public void ClickGuest(View view) {
+        startActivity(new Intent(MainActivity.this, ReadAllHerbActivity.class));
+    }
+}   // main class
